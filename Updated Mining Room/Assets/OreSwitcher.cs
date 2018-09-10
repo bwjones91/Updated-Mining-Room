f25b2d9afc2be9c439578418c505c2125d823fd5
bwjones91 <bwjones1991@gmail.com>
2018-09-10 18:17:02 -0400

    Arduino Communication began

---
+++ Updated Mining Room/Assets/OreSwitcher.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OreSwitcher : MonoBehaviour {
+    [SerializeField] float hitThreshold;
+
+    private float hitAmount = 10;
+    private ArduinoCommunicator AC;
+
+    void Start () {
+        AC = GameObject.Find("Arduino Communication").GetComponent<ArduinoCommunicator>();
+	}
+	
+	void Update () {
+        print(AC.GetMessageIN());
+        hitAmount = AC.GetMessageIN();
+
+        if(hitAmount > hitThreshold)
+        {
+            print("SUCCESS!");
+        }
+
+	}
+
+
+
+
+}
